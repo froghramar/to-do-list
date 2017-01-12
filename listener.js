1a41ec9e8487fb8849f2ce6bf5d6ea9232d67786
Feroz Ahmmed <froghramar@gmail.com>
2017-01-12 14:31:10 +0600

    bit of work

---
+++ listener.js
@@ -0,0 +1,7 @@
+var totalTasks = 0;
+
+function addListener() {
+    var title = document.getElementsByName('title')[0].value;
+    var description = document.getElementsByName('title')[0].value;
+    alert(title);
+}
