099ef2f9753ac177dea18bbd7f05de95f07bde47
Feroz Ahmmed <froghramar@gmail.com>
2017-01-13 17:24:34 +0600

    task list view added

--- listener.js
+++ listener.js
@@ -1,15 +1,30 @@
 var totalTasks = 0;
 
 $(document).ready(function(){
+    $("inputdiv").hide();
     $("button").click(function () {
         $("inputdiv").slideToggle();
     });
+    $("input[type='checkbox']").change(function() {
+        if(this.checked) {
+            this.parentNode.style.backgroundColor = 'cyan';
+        }
+        else{
+            this.parentNode.style.backgroundColor = 'white';
+        }
+    });
 });
 
-function addListener() {
+function newListener() {
+    document.getElementById('new').disabled = true;
+}
 
+function addListener() {
+    document.getElementById('new').disabled = false;
+    document.getElementById('counter').innerHTML = "[ " + (++totalTasks) + " tasks ]";
+    var title = document.getElementById('input-title');
 }
 
 function canceListener() {
-
+    document.getElementById('new').disabled = false;
 }
