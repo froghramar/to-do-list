6e99c7aed33e107348aff63fbf36f48de80fcbe9
Feroz Ahmmed <froghramar@gmail.com>
2017-01-15 14:00:41 +0600

    jquery removed

--- listener.js
+++ listener.js
@@ -1,12 +1,5 @@
 var totalTasks = 0;
 
-$(document).ready(function(){
-    $("inputdiv").hide();
-    $("button").click(function () {
-        $("inputdiv").slideToggle();
-    });
-});
-
 function checkboxClicked(box) {
     if(box.checked){
         box.parentNode.style.backgroundColor = 'cyan';
@@ -18,44 +11,66 @@ function checkboxClicked(box) {
 
 function enableNewButton(){
     document.getElementById('new').disabled = false;
-    $('#new').css('cursor','default');
+    document.getElementById("new").style.cursor = "default";
 }
 
 function disableNewButton(){
     document.getElementById('new').disabled = true;
-    $('#new').css('cursor','not-allowed');
+    document.getElementById("new").style.cursor = "not-allowed";
 }
 
 function newListener() {
     disableNewButton();
+    document.getElementById('inputdiv').style.display = "block";
+}
+
+function clearInput() {
+    document.getElementById('input-title').value = "";
+    document.getElementById('input-description').value = "";
 }
 
 function addListener() {
     enableNewButton();
+    document.getElementById('inputdiv').style.display = "none";
     document.getElementById('counter').innerHTML = "[ " + (++totalTasks) + " tasks ]";
     var title = document.getElementById('input-title').value;
     var description = document.getElementById('input-description').value;
     addTask(title, description);
+    clearInput();
 }
 
 function canceListener() {
     enableNewButton();
+    document.getElementById('inputdiv').style.display = "none";
+    clearInput();
+}
+
+function createTaskTitle(title) {
+    var tasktitle = document.createElement("task-title");
+    var titletext = document.createTextNode(title);
+    tasktitle.appendChild(titletext);
+    return tasktitle;
+}
+
+function createTaskDescription(description) {
+    var taskdescription = document.createElement("p");
+    var descriptiontext = document.createTextNode(description);
+    taskdescription.appendChild(descriptiontext);
+    return taskdescription;
+}
+
+function createCheckbox() {
+    var checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.onclick = function(){checkboxClicked(checkbox);};
+    return checkbox;
 }
 
 function addTask(title, description) {
     var task = document.createElement("div");
     task.className = "task";
-    var tasktitle = document.createElement("task-title");
-    var titletext = document.createTextNode(title);
-    tasktitle.appendChild(titletext);
-    var checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.onclick = function(){checkboxClicked(checkbox);};
-    var taskdescription = document.createElement("p");
-    var descriptiontext = document.createTextNode(description);
-    taskdescription.appendChild(descriptiontext);
-    task.appendChild(tasktitle);
-    task.appendChild(checkbox);
-    task.appendChild(taskdescription);
+    task.appendChild(createTaskTitle(title));
+    task.appendChild(createCheckbox());
+    task.appendChild(createTaskDescription(description));
     document.getElementById('list').appendChild(task);
 }
